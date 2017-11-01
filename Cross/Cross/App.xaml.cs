81e9d6eaac7153b2bb1a436f3a6a44d2d360697c
mehrdadjavidan <31864084+mehrdadjavidan@users.noreply.github.com>
2017-11-01 23:14:07 +0330

    Newtonsoft.Json Added

--- Cross/Cross/App.xaml.cs
+++ Cross/Cross/App.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Newtonsoft.Json;
 
 using Xamarin.Forms;
 
@@ -9,11 +10,21 @@ namespace Cross
 {
     public partial class App : Application
     {
+        public class Person
+        {
+            public string Name { get; set; }
+            public DateTime Birthday { get; set; }
+        }
+
         public App()
         {
             InitializeComponent();
 
             MainPage = new Cross.MainPage();
+
+            var person = new Person { Name = "Bob", Birthday = new DateTime(1987, 2, 2) };
+            var json = JsonConvert.SerializeObject(person);
+            var person2 = JsonConvert.DeserializeObject<Person>(json);
         }
 
         protected override void OnStart()
