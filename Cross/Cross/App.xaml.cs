f4b7e58a249561270c40f5cbf3667d177e1e3eda
Mehrdad Javidan <31864084+mehrdadjavidan@users.noreply.github.com>
2017-11-02 10:19:20 +0330

    Welcome 4

--- Cross/Cross/App.xaml.cs
+++ Cross/Cross/App.xaml.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using Newtonsoft.Json;
 
 using Xamarin.Forms;
 
@@ -10,21 +9,13 @@ namespace Cross
 {
     public partial class App : Application
     {
-        public class Person
-        {
-            public string Name { get; set; }
-            public DateTime Birthday { get; set; }
-        }
+        
 
         public App()
         {
             InitializeComponent();
 
             MainPage = new Cross.MainPage();
-
-            var person = new Person { Name = "Bob", Birthday = new DateTime(1987, 2, 2) };
-            var json = JsonConvert.SerializeObject(person);
-            var person2 = JsonConvert.DeserializeObject<Person>(json);
         }
 
         protected override void OnStart()
--- Cross/Cross/MainPage.xaml.cs
+++ Cross/Cross/MainPage.xaml.cs
@@ -4,14 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Newtonsoft.Json;
 
 namespace Cross
 {
     public partial class MainPage : ContentPage
     {
+        public class Person
+        {
+            public string Name { get; set; }
+            public DateTime Birthday { get; set; }
+        }
+
         public MainPage()
         {
             InitializeComponent();
+            
+            var person = new Person { Name = "Bob", Birthday = new DateTime(1987, 2, 2) };
+            var json = JsonConvert.SerializeObject(person);
+            var person2 = JsonConvert.DeserializeObject<Person>(json);
+
+            lblTitle.Text = "Welcome 4";
         }
     }
 }
