d502e05b2333ccbf4a0430e99e05d9427e9b482a
Mehrdad Javidan <31864084+mehrdadjavidan@users.noreply.github.com>
2017-11-05 11:25:42 +0330

    Welcome 6

--- Cross/Cross/MainPage.xaml.cs
+++ Cross/Cross/MainPage.xaml.cs
@@ -26,7 +26,7 @@ namespace Cross
             var json = JsonConvert.SerializeObject(person);
             var person2 = JsonConvert.DeserializeObject<Person>(json);
 
-            lblTitle.Text = "Welcome 5";
+            lblTitle.Text = "Welcome 6";
         }
     }
 }
